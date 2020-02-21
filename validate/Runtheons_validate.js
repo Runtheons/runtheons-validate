79110a0430423a7131e253b51f371e1bac20ed5b
iamousseni <ousseni.workg@gmail.com>
2020-02-21 09:49:30 +0100

    init commit

---
+++ validate/Runtheons_validate.js
@@ -0,0 +1,19 @@
+/**
+ *  example
+ *  id : RuntheonsValidate.int.length(90).notNull()
+ *  distance: RuntheonsValidate.int.null()
+ *  time: RuntheonsValidate.time.format('H:M:S').null()
+ *  description: RuntheonsValidate.text.null()
+ *  date: RuntheonsValidate.date.format('Y-m-d').null()
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+class Runtheons_validate{
+  
+}
