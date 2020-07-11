7005eac6f5639f9d0a8c9ed0e1df7c98c666cd46
Zexal0807 <gallinar00@gmail.com>
2020-07-11 15:51:15 +0200

    Some fix

--- validate/Runtheons_validate.js
+++ validate/Runtheons_validate.js
@@ -5,14 +5,13 @@
  *  @version 1.1.1.0
  */
 
-import {data} from './data.js';
-
+const data = require('./data.js');
 module.exports = class RuntheonsValidate {
 
 	validate(objSchema, objData) {
-		let errors = [];
+		var errors = [];
 		for (const property in objSchema) {
-			_val(property, objSchema[property], objData[property], &errors);
+			this._val(property, objSchema[property], objData[property], errors);
 		}
 		return this.isEmptyObj(errors) ? { result: true, errors: errors } : { result: false, errors: errors };
 	}
@@ -20,35 +19,41 @@ module.exports = class RuntheonsValidate {
 	_val(property, objSchema, objData, errors){
 		if(objSchema['type'] == undefined){
 			errors.push(property+" haven't 'type' parameter");
-			return false;
-		}	
-		let error = {};
+		}
 		switch(objSchema['type']){
 			case 'array':
-				//controllo se davvero è un array
-				if(!Array.isArray(objData)){
-					errors.push(property+" is not an Array");
-					return false;
+				if(objSchema['required'] == undefined || objSchema['required'] == true){
+					if(objData == undefined){
+						errors.push(property+" is required");
+					}
+					//controllo se davvero è un array
+					if(!Array.isArray(objData)){
+						errors.push(property+" is not an Array");
+					}
+					//se davvero è un array controlo se c'è il parametro obbligatorio of
+					if(objSchema.of == undefined){
+						errors.push(property+" haven't 'of' parameter");
+					}
+					//Controllo che tutti gli elementi siano dello stesso schema indicato in objSchema.of
+					for(let i = 0; i < objData.length; i++){
+						this._val(property+"."+i, objSchema.of, objData[i], errors);
+					}
 				}
-				//se davvero è un array controlo se c'è il parametro obbligatorio of
-				if(objSchema.of == undefined){
-					errors.push(property+" haven't 'of' parameter");
-					return false;
-				}
-				//Controllo che tutti gli elementi siano dello stesso schema indicato in objSchema.of
-				for(let i = 0, i < objData.length; i++){
-					if(!_val(i, objSchema.of, objData[i]), &errors)
-						return false;
-				}
-				return true;
+				break;
 			case 'int':
-				return data.int.validate(property, objSchema, objData, &errors);
+				data.int.validate(property, objSchema, objData, errors);
+				break;
 			case 'float':
-				return data.float.validate(property, objSchema, objData, &errors);
+				data.float.validate(property, objSchema, objData, errors);
+				break;
 			case 'double':
-				return data.double.validate(property, objSchema, objData, &errors);
+				data.double.validate(property, objSchema, objData, errors);
+				break;
+			case 'string':
+				data.string.validate(property, objSchema, objData, errors);
+				break;
 			default:
-				return false;
+				errors.push(property+" type is unknown");
 		}
 	}
 	
--- validate/data.js
+++ validate/data.js
@@ -6,85 +6,78 @@
 
 //Definizione di un generale valore
 class superData{
-	function validate(property, schema, value, errors){
+	validate(property, schema, value, errors){
 		let ret = true;
 		for (const pp in schema) {
 			switch(pp){
 				case 'min':
-					ret = this.min(property, schema, value, &errors);
+					ret = this.min(property, schema, value, errors);
 					break
 				case 'max':
-					ret = this.min(property, schema, value, &errors);
+					ret = this.max(property, schema, value, errors);
 					break
 				case 'required':
-					ret = this.required(property, schema, value, &errors);
+					ret = this.required(property, schema, value, errors);
 					break
 				default:
 					break;
 			}
-			if(!ret)
-				return false;
 		}
-		return true;
 	}
-	function max(property, schema, value, errors){
-		if(value <= schema['max']{
-			errors.push(property+" is not greater than "+schema['max']);
-			return false;
+	
+	min(property, schema, value, errors){
+		if(value < schema['min']){
+			errors.push(property+" is lower than "+schema['min']);
 		}
-		return true;
 	}
-	function required(property, schema, value, errors){
+	
+	max(property, schema, value, errors){
+		if(value > schema['max']){
+			errors.push(property+" is greater than "+schema['max']);
+		}
+	}
+	
+	required(property, schema, value, errors){
 		if(value == undefined || value == null || value == ""){
 			errors.push(property+" is required");
-			return false;
 		}
-		return true;
 	}
-	abstract function required(property, schema, value, errors);
+	
 }
 //Numeric data (int, double, float..)
 class numberData extends superData{
-	function validate(property, schema, value, errors){
-		if(typeof value = "number"){
+	validate(property, schema, value, errors){
+		if(typeof value != "number"){
 			errors.push(property+" is not a number");
-			return false;
 		}else
-			super.validate(property, schema, value, &errors);
-	}
-	function required(property, schema, value, errors){
-		if(value == undefined || value == null || value == ""){
-			errors.push(property+" is required");
-			return false;
-		}
-		return true;
-	}
-}
-//String data (string, date, datetime, ip, mac, email.....)
-class stringData extends superData{
-	function required(property, schema, value, errors){
-			errors.push(property+" is required");
-			return false;
-		}
-		return true;
+			super.validate(property, schema, value, errors);
 	}
 }
 
-exports.data = {
-	int : class intData extends numberData{
+//String data (string, date, datetime, ip, mac, email.....)
+class stringData extends superData{
+	validate(property, schema, value, errors){
+		if(typeof value != "string"){
+			errors.push(property+" is not a string");
+		}else
+			super.validate(property, schema, value, errors);
+	}
+}
+
+module.exports = {
+	int : new class intData extends numberData{
 		//Per gli int controllo che non abbia parte decimale dividendo per 1
-		function validate(property, schema, value, errors){
+		validate(property, schema, value, errors){
 			if(value%1 != 0){
 				errors.push(property+" is not int");
-				return false;
 			}else
-			super.validate(property, schema, value, &errors);
+			super.validate(property, schema, value, errors);
 		}
 	},
-	float : class floatData extends numberData{},
-	double : class doubleData extends numberData{},
-	string : stringData,
-	date : class dateData extends stringData{},
-	datetime : class datetimeData extends stringData{}
+	float : new class floatData extends numberData{},
+	double : new class doubleData extends numberData{},
+	string : new stringData,
+	date : new class dateData extends stringData{},
+	datetime : new class datetimeData extends stringData{}
 	//Here we can add new type
 };
